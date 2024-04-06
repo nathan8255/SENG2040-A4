ed4a2fcecfe224fef7e130e9b8f5326df078c151
nathan8255 <njoannette8255@conestogac.on.ca>
2024-04-06 02:10:23 -0400

    added static root to settings.py

--- posts_proj/settings.py
+++ posts_proj/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = 'home/njoannette8255/SENG2040-A4/static'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
